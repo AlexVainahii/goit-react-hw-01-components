2e404e24a27729a9ae72029815d7923e93f23b6b
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-11-27 15:07:29 +0200

    sds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,26 @@
-import {Profile} from './Profile';
+import {Profile} from './Profile/Profile';
+import {Statistics} from './Statistics/Statistics';
 import user from '../user.json'
+import data from '../data.json';
+import React from 'react';
 export const App = () => {
   return (
     <>
-    <div>  <Profile user={user}/></div>
-    
+    <h2>Task1</h2>
+     <Profile 
+       username={user.username}
+       tag={user.tag}
+       location={user.location}
+       avatar={user.avatar} 
+       followers={user.stats.followers}
+       views={user.stats.views} 
+      likes={user.stats.likes}
+     />
+     <h2>Task2</h2>
+    <Statistics title="Upload stats" stats={data} />
+    <Statistics stats={data} />
+     <h2>Task3</h2>
+     <h2>Task4</h2>
     </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import css from "./Statistics.module.css";
+export const Statistics=(  {
+  title,
+  stats,
+} )=> {
+  return( <section class="statisstic">
+  {title &&<h2 class="title">{title}</h2>}
+  <ul class="stat-list">
+   {stats.map(stat => {
+   return(
+    <li class="item">
+      <span class="label">{stat.label}</span>
+      <span class="percentage">{stat.percentage}%</span>
+    </li>
+    )})}
+    </ul>   
+</section>
+)}
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(PropTypes.object).isRequired,
+}; 
