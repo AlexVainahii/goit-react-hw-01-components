dc5c3064106620e246d9adebf93a99ec1058c420
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-12-03 08:27:01 +0200

    finish fix1

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import css from './FriendListItem.module.css';
-export const FriendListItem = ({ key, avatar, name, isOnline }) => {
+export const FriendListItem = ({ id, avatar, name, isOnline }) => {
   return (
-    <li key={key} className={css.item}>
+    <li key={id} className={css.item}>
       <span className={clsx(isOnline ? css.online : css.offline)}></span>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
